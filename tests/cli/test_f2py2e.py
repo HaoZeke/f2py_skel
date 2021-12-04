0b3392a877932a2cfbcc227ce45e3554867e428f
Rohit Goswami <rog32@hi.is>
2021-12-04 13:24:04 +0000

    TST: Add helpers

--- tests/cli/test_f2py2e.py
+++ tests/cli/test_f2py2e.py
@@ -1,7 +1,9 @@
-import pytest
 import textwrap
 from pathlib import Path
 from unittest.mock import patch
+from collections import namedtuple
+
+import pytest
 
 import numpy as np
 from numpy.testing import assert_, assert_equal, IS_PYPY
@@ -9,14 +11,60 @@ from numpy.testing import assert_, assert_equal, IS_PYPY
 from .. import util
 from f2py_skel.frontend import main as f2pycli
 
+#########################
+# CLI utils and classes #
+#########################
+
+PPaths = namedtuple("PPaths",
+                    "finp, f90inp, pyf, wrap77, wrap90, cmodf")
+
+def get_io_paths(fname_inp, mname="untitled"):
+    """Takes in a temporary file for testing and returns the expected output and input paths
+
+    Here expected output is essentially one of any of the possible generated
+    files.
+
+    ..note::
+
+         Since this does not actually run f2py, none of these are guaranteed to
+         exist, and module names are typically incorrect
+
+    Parameters
+    ----------
+    fname_inp : str
+                The input filename
+    mname : str, optional
+                The name of the module, untitled by default
+
+    Returns
+    -------
+    genp : NamedTuple PPaths
+            The possible paths which are generated, not all of which exist
+    """
+    bpath = Path(fname_inp)
+    bstem = bpath.stem
+    foutl = PPaths(
+        finp = bpath.with_suffix(".f"),
+        f90inp = bpath.with_suffix(".f90"),
+        pyf = bpath.with_suffix(".pyf"),
+        wrap77 = bpath.with_name(f"{mname}-f2pywrappers.f"),
+        wrap90 = bpath.with_name(f"{mname}-f2pywrappers2.f90"),
+        cmodf = bpath.with_name(f"{mname}module.c")
+    )
+    return foutl
+
+##########################
+# CLI Fixtures and Tests #
+##########################
+
 @pytest.fixture(scope="session")
 def hello_world_f90(tmpdir_factory):
     """Generates a single f90 file for testing
     """
     fdat = textwrap.dedent("""
-    program main
+    function hi
       print*, "Hello World"
-    end program main
+    end function
     """)
     fn = tmpdir_factory.getbasetemp() / "hello.f90"
     fn.write_text(fdat, encoding="ascii")
