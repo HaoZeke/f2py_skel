5ab3f8a1976e3cdbaf67a7e1519f05f0a8a2db9a
Rohit Goswami <rog32@hi.is>
2021-12-13 00:41:42 +0000

    MAINT: Add a deprecation helper

--- f2py_skel/__init__.py
+++ f2py_skel/__init__.py
@@ -1,5 +1,7 @@
 """Skeleton Fortran to Python Interface Generator."""
 
+__all__ = ['run_main', 'compile', 'get_include']
+
 from f2py_skel import frontend
 from f2py_skel import __version__
 # Helpers
@@ -8,5 +10,11 @@ from f2py_skel.utils.pathhelper import get_include
 from f2py_skel.utils.npdist import compile
 from f2py_skel.frontend.f2py2e import main
 
+run_main = frontend.f2py2e.run_main
+
 if __name__ == "__main__":
     sys.exit(main())
+
+# Deprecated
+from f2py_skel.utils.deprecationhelpers import PathDeprecationLoader
+crackfortran = PathDeprecationLoader("crackfortran", "f2py_skel.frontend.crackfortran", warnmsg="DO NOT USE")
---
+++ f2py_skel/utils/deprecationhelpers.py
@@ -0,0 +1,66 @@
+import typing, types
+import sys, warnings, importlib
+
+
+class PathDeprecationLoader(types.ModuleType):
+    """
+    Based off of:
+     https://github.com/tensorflow/tensorflow/blob/v2.2.0/tensorflow/python/util/lazy_lloader.py
+     https://docs.python.org/3/library/importlib.html
+    Lazily import a module, this allows for throwing warnings when deprecated locations are used.
+
+    Usage:
+    # __init__.py
+    crackfortran = PathDeprecationLoader("crackfortran", "f2py_skel.frontend.crackfortran", warning="DO NOT USE")
+    # Callee
+    from f2py_skel import crackfortran
+    from crackfortran import markinnerspaces
+
+    .. note::
+
+       In its current implementation
+
+           from f2py_skel.crackfortran import markinnerspaces
+
+       Will fail.
+
+    """
+    def __init__(self,
+                 local_name: str,
+                 name: str,
+                 warnmsg: typing.Optional[str] = None):
+        self._local_name = local_name
+        self._warnmsg = warnmsg
+        self._parent_scope = globals()
+
+        super(PathDeprecationLoader, self).__init__(name)
+
+        self._module = self._lload()
+
+    def _lload(self) -> types.ModuleType:
+        """Load the module lazily."""
+        spec = importlib.util.find_spec(self.__name__)
+        loader = importlib.util.LazyLoader(spec.loader)
+        spec.loader = loader
+        module = importlib.util.module_from_spec(spec)
+        sys.modules[self._local_name] = module
+        self._parent_scope[self._local_name] = module
+        loader.exec_module(module)
+
+        # Emit a warning if one was specified
+        if self._warnmsg:
+            warnings.warn(self._warnmsg, PendingDeprecationWarning)
+            # Make sure to only warn once.
+            self._warnmsg = None
+
+        self._module = module
+        return module
+
+    def __getattr__(self, item):  # type: ignore[no-untyped-def]
+        return getattr(self._module, item)
+
+    def __hasattr__(self, item):  # type: ignore[no-untyped-def]
+        return hasattr(self._module, item)
+
+    def __dir__(self) -> typing.List[str]:
+        return dir(self._module)
