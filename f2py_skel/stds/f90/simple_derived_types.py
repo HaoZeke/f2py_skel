d36965a48f633d3947a6c53c9efa90d7f0cdf0ac
Rohit Goswami <rog32@hi.is>
2022-01-01 13:49:22 +0000

    TMP: Update to a more rational approach

--- f2py_skel/stds/f90/simple_derived_types.py
+++ f2py_skel/stds/f90/simple_derived_types.py
@@ -11,6 +11,8 @@ NO WARRANTY IS EXPRESSED OR IMPLIED.  USE AT YOUR OWN RISK.
 
 """
 
+from collections import namedtuple
+
 fcpyfunc = {
     'c_double': "PyFloat_AsDouble",
     'c_float': "PyFloat_AsDouble",
@@ -21,6 +23,45 @@ fcpycode = {
     'c_double': 'd',
 }
 
+FCPyConversionRow = namedtuple('FCPyConversionRow', ['fortran_isoc', 'ctype', 'py_type', 'py_conv'])
+
+# These are sourced from:
+# ISO_C_BINDINGS: https://gcc.gnu.org/onlinedocs/gfortran/ISO_005fC_005fBINDING.html
+# Python Types: https://docs.python.org/3/c-api/arg.html
+# Py_Conv: https://docs.python.org/3/c-api/
+fcpyconv = [
+    # Integers
+    FCPyConversionRow(fortran_isoc = 'c_int',
+                      ctype = 'int',
+                      py_type = 'i',
+                      py_conv = 'PyLong_FromLong'),
+    FCPyConversionRow(fortran_isoc = 'c_short',
+                      ctype = 'short int',
+                      py_type = 'h',
+                      py_conv = 'PyLong_FromLong'),
+    FCPyConversionRow(fortran_isoc = 'c_long',
+                      ctype = 'long int',
+                      py_type = 'l',
+                      py_conv = 'PyLong_FromLong'),
+    FCPyConversionRow(fortran_isoc = 'c_long_long',
+                      ctype = 'long long int',
+                      py_type = 'L',
+                      py_conv = 'PyLong_FromLongLong'),
+    # TODO: Add int6 and other sizes
+    # Evidently ISO_C_BINDINGs do not have unsigned integers
+    # Floats
+    FCPyConversionRow(fortran_isoc = 'c_float',
+                      ctype = 'float',
+                      py_type = 'f',
+                      py_conv = 'PyFloat_AsDouble'),
+    FCPyConversionRow(fortran_isoc = 'c_double',
+                      ctype = 'double',
+                      py_type = 'd',
+                      py_conv = 'PyFloat_AsDouble'),
+]
+
+def equiv_type(from_lang, to_lang):
+    pass
 
 def find_typeblocks(pymod):
     """Return a list of type definitions
@@ -49,6 +90,7 @@ def buildhooks(pymod):
     res = []
     dret = []
     dretf = []
+    # XXX: Get the type definitions in a sane way
     for typedet in pymod.get('body')[0]['body'][0]['body']:
         defs = []
         fkinds = []
