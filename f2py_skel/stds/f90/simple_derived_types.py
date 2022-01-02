a454e81d285381f00897a4475771204909bf0145
Rohit Goswami <rog32@hi.is>
2022-01-02 18:19:42 +0000

    ENH: Update to be more generic

--- f2py_skel/stds/f90/simple_derived_types.py
+++ f2py_skel/stds/f90/simple_derived_types.py
@@ -23,7 +23,7 @@ fcpycode = {
     'c_double': 'd',
 }
 
-FCPyConversionRow = namedtuple('FCPyConversionRow', ['fortran_isoc', 'ctype', 'py_type', 'py_conv'])
+FCPyConversionRow = namedtuple('FCPyConversionRow', ['fortran_isoc', 'ctype', 'py_type', 'py_conv', 'varname'], defaults = [None])
 
 # These are sourced from:
 # ISO_C_BINDINGS: https://gcc.gnu.org/onlinedocs/gfortran/ISO_005fC_005fBINDING.html
@@ -60,9 +60,6 @@ fcpyconv = [
                       py_conv = 'PyFloat_AsDouble'),
 ]
 
-def equiv_type(from_lang, to_lang):
-    pass
-
 def find_typeblocks(pymod):
     """Return a list of type definitions
 
@@ -76,7 +73,6 @@ def find_typeblocks(pymod):
     ret : list
        This returns a list of module blocks
     """
-    breakpoint()
     ret = []
     m = pymod.get('body')
     for blockdef in m:
@@ -85,53 +81,38 @@ def find_typeblocks(pymod):
             ret.append(blockdef)
     return ret
 
+def recursive_lookup(k, d):
+    if k in d: return d[k]
+    for v in d.values():
+        if isinstance(v, dict):
+            a = recursive_lookup(k, v)
+            if a is not None: return a
+    return None
 
-def buildhooks(pymod):
-    res = []
-    dret = []
-    dretf = []
-    # XXX: Get the type definitions in a sane way
-    for typedet in pymod.get('body')[0]['body'][0]['body']:
-        defs = []
-        fkinds = []
-        cstruct_var = []
-        pyc_conv_fn = []
-        for var in [
-                x for x in typedet.get('vars')
-                if 'kindselector' in typedet['vars'][x].keys()
-        ]:
-            if 'c_' not in typedet['vars'][var]['kindselector']['kind']:
-                continue
-            else:
-                vkind = typedet['vars'][var]['kindselector']['kind']
-                fkinds.append(vkind)
-                ckind = vkind.replace('c_', '')
-                vdef = f"{ckind} {var};"
-                cstruct_var.append(var)
-                pyc_conv_fn.append(fcpyfunc.get(vkind))
-                defs.append(vdef)
-        if len(defs) > 0:
-            cline = []
-            structname = typedet['name']
-            # XXX: Ugly hack, this forces
-            # capi_buildvalue = Py_BuildValue("{s:f,s:f,s:f}","x", array.x,
-            # "y", array.y,
-            # "z", array.z -> Note the missing ,
-            # );
-            for i in range(len(defs)):
-                dretf.append(f"s:{fcpycode.get(fkinds[i])}")
-                # TODO: Stop harcoding array
-                dret.append(f"""{',' if i==0 else ''}\
-\t \"{cstruct_var[i]}\", array.{cstruct_var[i]}{'' if i==len(defs)-1 else ','}
-                """)
-                # TODO: Stop hardcoding vals
-                cline.append(
-                    f"xstruct->{cstruct_var[i]} = {pyc_conv_fn[i]}(PyList_GetItem(vals, {i}));\n            "
-                )
-            res.append(f"""
+def extract_typedat(typeblock):
+    assert typeblock['block'] == 'type'
+    typevars = []
+    structname = typeblock['name']
+    for vname in typeblock['varnames']:
+        vfkind = typeblock['vars'][vname]['kindselector']['kind']
+        tvar = [x for x in fcpyconv if x.fortran_isoc == vfkind][0]
+        typevars.append(tvar._replace(varname = vname))
+    return structname, typevars
+
+def gen_typedecl(structname, tvars):
+    vdefs = [''.join(f"{x.ctype} {x.varname};") for x in tvars]
+    tdecl = f"""
     typedef struct {{
-    {''.join(defs)}
+    {' '.join(vdefs)}
     }} {structname};
+    """
+    return tdecl
+
+def gen_typefunc(structname, tvars):
+    cline = []
+    for idx,tv in enumerate(tvars):
+        cline.append(f"xstruct->{tv.varname} = {tv.py_conv}(PyList_GetItem(vals, {idx}));\n\t\t")
+    rvfunc = f"""
     int try_pyarr_from_{structname}({structname} *xstruct, PyObject *x_capi){{
             PyObject* dict;
             PyArg_ParseTuple(x_capi, "O", &dict);
@@ -139,11 +120,47 @@ def buildhooks(pymod):
             {''.join(cline)}
             return 1;
             }}
-    """)
+    """
+    return rvfunc
+
+def gen_typeret(structname, tvars):
+    """
+    The return value is generated from:
+        capi_buildvalue = Py_BuildValue(\"#returnformat#\"#return#);
+    Mapping:
+    returnformat -> retvardecl
+    return -> dretlines
+    """
+    retvardecl = f"{{{','.join([f's:{x.py_type}' for x in tvars])}}}"
+    dretlines = []
+    for idx,tv in enumerate(tvars):
+        # XXX: Ugly hack, this forces
+        # capi_buildvalue = Py_BuildValue("#returnformat#","x", array.x,
+        # "y", array.y,
+        # "z", array.z # -> Note the missing ,
+        # );
+        dretlines.append(f"""{',' if idx==0 else ''}\
+\t \"{tv.varname}\", array.{tv.varname}\
+{'' if idx==len(tvars)-1 else ','}
+        """)
+    return retvardecl, ''.join(dretlines)
+
+def buildhooks(pymod):
+    # One structure and function for each derived type
+    res = []
+    # XXX: Get the type definitions in a sane way
+    for typedet in pymod.get('body')[0]['body'][0]['body']:
+        if typedet['block']!='type':
+            continue
+        sname, vardefs = extract_typedat(typedet)
+        res.append('\n'.join([gen_typedecl(sname, vardefs), gen_typefunc(sname, vardefs)]))
+        dretf, dret  = gen_typeret(sname, vardefs)
     # TODO: Document how these dictionary items get used in rules.py
+    # TODO: The return values are based on the subroutine being considered, not
+    # on the derived type
     ret = {
         'typedefs_derivedtypes': res,
-        'derived_returnformat': f"{{{','.join(dretf)}}}",
+        'derived_returnformat': dretf,
         'derived_return': dret
     }
     return ret
